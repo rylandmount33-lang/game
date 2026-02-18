0cd17cf91581456f7e720e57bb4a2a21658c3487
rylandmount33-lang <ryland.mount.33@stu.tumwater.k12.wa.us>
2026-02-18 11:57:20 -0800

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,108 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "ryduck":return img`
+. . . . . . . . . . b 5 b . . . 
+. . . . . . . . . b 5 b . . . . 
+. . . . . . b b b b b b . . . . 
+. . . . . b b 5 5 5 5 5 b . . . 
+. . . . b b 5 d 1 8 5 d 4 c . . 
+. . . . b 5 5 1 8 8 d d 4 4 4 b 
+. . . . b 5 5 d 8 b 4 4 4 4 b . 
+. . . b d 5 5 5 5 4 4 4 4 b . . 
+. b b d d d 5 5 5 5 5 5 5 b . . 
+b d d d b b b 5 5 5 5 5 5 5 b . 
+c d d b 5 5 d c 5 5 5 5 5 5 b . 
+c b b d 5 d c d 5 5 5 5 5 5 b . 
+c b 5 5 b c d d 5 5 5 5 5 5 b . 
+b b c c c d d d 5 5 5 5 5 d b . 
+. . . . c c d d d 5 5 5 b b . . 
+. . . . . . c c c c c b b . . . 
+`;
+            case "image2":
+            case "jonshark":return img`
+.................ccfff..............
+................cddbbf..............
+...............cddbbf...............
+..............fccbbcf............ccc
+........ffffffccccccff.........ccbbc
+......ffbbbbbbbbbbbbbcfff.....cdbbc.
+....ffbbbbbbbbbcbcbbbbcccff..cddbbf.
+....fbcbbbbbffbbcbcbbbcccccfffdbbf..
+....fbbb1111ff1bcbcbbbcccccccbbbcf..
+.....fb11111111bbbbbbcccccccccbccf..
+......fccc33cc11bbbbccccccccfffbbcf.
+.......fc131c111bbbcccccbdbc...fbbf.
+........f33c111cbbbfdddddcc.....fbbf
+.........ff1111fbdbbfddcc........fff
+...........cccccfbdbbfc.............
+.................fffff..............
+`;
+            case "image4":
+            case "coin":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . 5 5 5 5 . . . . . . . 
+. . . . 5 5 4 5 5 5 . . . . . . 
+. . . . 5 5 4 5 5 5 . . . . . . 
+. . . 5 5 5 4 4 5 5 5 . . . . . 
+. . . 5 5 5 4 4 5 5 5 . . . . . 
+. . . 5 5 5 5 4 5 5 5 . . . . . 
+. . . 5 5 5 5 4 5 5 5 . . . . . 
+. . . . 5 5 5 4 5 5 . . . . . . 
+. . . . . 5 5 5 5 . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image3":
+            case "brick":return img`
+. . . . 3 . . . . . . . . . . 
+. . . . . 3 3 . . 3 . . . . . 
+. . . . . . 3 3 3 . . . . . . 
+. . . . 8 8 8 8 8 8 8 . . . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 . . 
+. 3 3 3 3 8 8 8 8 8 3 3 3 3 . 
+3 9 9 9 9 3 8 8 8 3 9 9 9 9 3 
+9 c c 8 8 9 3 8 3 9 8 8 c c 9 
+c 8 8 1 1 8 9 3 9 8 1 1 8 8 c 
+a 8 1 9 f 1 8 9 8 1 9 f 1 8 a 
+a 8 1 f f 1 8 c 8 1 f f 1 8 a 
+a a 8 1 1 8 c 9 c 8 1 1 8 a a 
+a a a 8 8 a a a a a 8 8 a a a 
+a a a a a a a a a a a a a a a 
+8 a 8 a 8 a 8 a 8 a 8 a 8 a 8 
+. 8 . 8 . 8 . 8 . 8 . 8 . 8 . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
